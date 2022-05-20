917a0ccbe3fb0a2a424ddf41edede6eae05cc79d
Ludo Pulles <ludo.pulles@gmail.com>
2022-05-20 18:34:25 +0200

    80-bit precision gaussian sampler

--- tests/test_sampler.c
+++ tests/test_sampler.c
@@ -184,6 +184,106 @@ mkgauss_sign(prng *rng, uint8_t double_mu)
 	return 2 * w - (int) double_mu;
 }
 
+
+static const uint16_t gauss_hi[10] = {
+	0x580B, 0x35F9,
+	0x1D34, 0x0DD7,
+	0x05B7, 0x020C,
+	0x00A2, 0x002B,
+	0x000A, 0x0001,
+};
+static const uint64_t gauss_lo[26] = {
+	0x0C27920A04F8F267, 0x3C689D9213449DC9,
+	0x1C4FF17C204AA058, 0x7B908C81FCE3524F,
+	0x5E63263BE0098FFD, 0x4EBEFD8FF4F07378,
+	0x56AEDFB0876A3BD8, 0x4628BC6B23887196,
+	0x061E21D588CC61CC, 0x7F769211F07B326F,
+	0x2BA568D92EEC18E7, 0x0668F461693DFF8F,
+	0x00CF0F8687D3B009, 0x001670DB65964485,
+	0x000216A0C344EB45, 0x00002AB6E11C2552,
+	0x000002EDF0B98A84, 0x0000002C253C7E81,
+	0x000000023AF3B2E7, 0x0000000018C14ABF,
+	0x0000000000EBCC6A, 0x000000000007876E,
+	0x00000000000034CF, 0x000000000000013D,
+	0x0000000000000006, 0x0000000000000000,
+};
+
+static inline int8_t
+secure_gauss(prng *rng, uint8_t double_mu)
+{
+	uint16_t r_hi, p_hi;
+	uint64_t r_lo, p_lo;
+	uint8_t c, v, k, neg;
+
+	/*
+	 * We use 80 random bits to determine the value, by looking in the
+	 * cumulative probability table.
+	 */
+	r_hi = prng_get_u8(rng);
+	/*
+	 * However to do comparisons in constant between say a and b, we look at
+	 * the sign of (a - b) and therefore to compare the highest part of the
+	 * numbers, we only use 15 randomly sampled bits.
+	 */
+	r_hi = ((r_hi << 8) | prng_get_u8(rng)) & 0x7FFF;
+	r_lo = prng_get_u64(rng);
+	/*
+	 * Get the sign bit out of the lowest part
+	 */
+	neg = (uint8_t)(r_lo >> 63);
+	r_lo &= ~((uint64_t)1u << 63);
+
+	v = 0;
+	for (k = 5; k < 13; k ++) {
+		p_lo = gauss_lo[2 * k + double_mu];
+
+		/*
+		 * add 1 iff r_lo < p_lo.
+		 */
+		v += (uint8_t)((uint64_t)(r_lo - p_lo) >> 63);
+	}
+
+	/*
+	 * If r_hi > 0, set v to zero, otherwise leave v as is.
+	 */
+	v = v & -((r_hi - 1) >> 15);
+
+	for (k = 0; k < 5; k ++) {
+		p_lo = gauss_lo[2 * k + double_mu];
+		p_hi = gauss_hi[2 * k + double_mu];
+
+		/*
+		 * c = 1 if r_lo < p_lo, else c = 0
+		 */
+		c = (uint8_t)((uint64_t)(r_lo - p_lo) >> 63);
+
+		/*
+		 * if (r_hi < p_hi or (r_hi == p_hi and c == 1)) {
+		 *     c = 1;
+		 * } else {
+		 *     c = 0;
+		 * }
+		 */
+		c = (uint8_t)((uint16_t)(r_hi - p_hi - c) >> 15);
+		v += c;
+	}
+
+	/*
+	 * Multiply by two and apply the change in support:
+	 * If double_mu = 0, then v = 0,2,4,...
+	 * If double_mu = 1, then v = 1,3,5,...
+	 */
+	v = (v << 1) | double_mu;
+
+	/*
+	 * Apply the sign ('neg' flag). If neg = 0, this has no effect.
+	 * However, if neg = 1, this changes v into -v = (~v) + 1.
+	 */
+	v = (v ^ -neg) + neg;
+	return *(int8_t *)&v;
+
+}
+
 // =============================================================================
 
 const int num_samples = 10 * 1000 * 1000;
@@ -258,11 +358,11 @@ int main() {
 	for (int i = -50; i < 50; i++)
 		P[i + 50] /= norm[((unsigned) i) & 1];
 
-	for (int coset = 0; coset < 2; coset++) {
+	for (int8_t coset = 0; coset < 2; coset++) {
 		memset(freq, 0, sizeof freq);
 
 		for (int i = num_samples; i--; ) {
-			int x = mkgauss_sign(&p, coset);
+			int x = secure_gauss(&p, coset);
 			freq[x + 50]++;
 		}
 
