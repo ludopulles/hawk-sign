9934ad281158510186f64ede1eb1710d5f14f6a8
Ludo Pulles <ludo.pulles@gmail.com>
2022-07-12 15:57:38 +0200

    Improve fixed point FFT: ~ 31 bits of precision

--- tests/test_forge.c
+++ tests/test_forge.c
@@ -56,28 +56,28 @@ void test_simple_forgeries(unsigned logn)
 
 
 
-const int num_keygen = 100;
-const int signs_per_kg = 100;
-const int totalruns = num_keygen * signs_per_kg;
+const size_t num_kg = 100, signs_per_kg = 100;
 
 int count_fails(unsigned logn)
 {
 	size_t num_fails = 0;
 
 	// Generate key pair.
-	for (size_t u = 0; u < num_keygen; u++) {
+	for (size_t u = 0; u < num_kg; u++) {
 		Zf(keygen)(&sc, f, g, F, G, iq00, iq10, logn, tmp.b);
+		// Zf(complete_pubkey)(iq00, iq10, q00, q10, q11, logn);
 		Zf(expand_seckey)(exp_sk, f, g, F, logn);
 
-		for (int rep = 0; rep < totalruns/num_keygen; rep++) {
+		for (size_t rep = 0; rep < signs_per_kg; rep++) {
 			// make a signature of a random message...
 			inner_shake256_extract(&sc, h, sizeof h);
 
 			// Compute the signature.
-			// while (!Zf(sign)(&sc, sig, exp_sk, h, logn, tmp.b)) {}
-			while (!Zf(sign_NTT)(&sc, sig, f, g, F, G, h, logn, tmp.b)) {}
+			while (!Zf(sign)(&sc, sig, exp_sk, h, logn, tmp.b)) {}
+			// while (!Zf(sign_NTT)(&sc, sig, f, g, F, G, h, logn, tmp.b)) {}
 
 			num_fails += !Zf(verify_NTT)(h, sig, iq00, iq10, logn, tmp.b);
+			// num_fails += !Zf(verify)(h, sig, q00, q10, q11, logn, tmp.b);
 		}
 	}
 	return num_fails;
@@ -97,12 +97,12 @@ int main() {
 	 * For smaller values of logn, an all-zeros signature might verify to a
 	 * certain hash, but we claim no security for them anyway.
 	 */
-	for (unsigned logn = 6; logn <= MAXLOGN; logn++) {
+	for (unsigned logn = 1; logn <= MAXLOGN; logn++) {
 		// printf("%d", logn);
 		// fflush(stdout);
 		// test_simple_forgeries(logn);
-		printf("%d: %d/%d fails\n",
-			logn, count_fails(logn), totalruns);
+		printf("%u: %d/%zu fails\n",
+			logn, count_fails(logn), num_kg * signs_per_kg);
 	}
 
 	printf("\nNo simple forgeries were possible.\n");
